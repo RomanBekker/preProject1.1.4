3a78ac336779516e402236de7dfc09bead1ca08f
Roman Bekker <bekker.roman@list.ru>
2022-10-20 18:21:32 +0300

    commit 2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,76 +3,92 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
 
     //Создать таблицу пользователей
     public void createUsersTable() {
-        Util util = new Util();
-        try {
-            PreparedStatement prepareStatement = util.getConnection().prepareStatement("create table if not exists table113 ( id bigint PRIMARY KEY AUTO_INCREMENT," +
-                    " name varchar(45),  lastName varchar(45),  age int);");
+        try (PreparedStatement prepareStatement = connection.prepareStatement("create table if not exists table113 ( id bigint PRIMARY KEY AUTO_INCREMENT," +
+                " name varchar(45),  lastName varchar(45),  age int);")) {
+            connection.setAutoCommit(false);
             prepareStatement.execute();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     //Удалить таблицу пользователей
     public void dropUsersTable() {
-        Util util = new Util();
-        try {
-            PreparedStatement prepareStatement = util.getConnection().prepareStatement("drop table if exists table113;");
+        try (PreparedStatement prepareStatement = connection.prepareStatement("drop table if exists table113;")) {
+            connection.setAutoCommit(false);
             prepareStatement.execute();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     //Сохранить пользователя (Create)
     public void saveUser(String name, String lastName, byte age) {
-        Util util = new Util();
-        try {
-            PreparedStatement prepareStatement = util.getConnection().prepareStatement("insert into table113 " +
-                    "(name, lastName, age) values (?, ?, ?);");
+        try (PreparedStatement prepareStatement = connection.prepareStatement("insert into table113 " +
+                "(name, lastName, age) values (?, ?, ?);")) {
+            connection.setAutoCommit(false);
             prepareStatement.setString(1, name);
             prepareStatement.setString(2, lastName);
             prepareStatement.setByte(3, age);
             prepareStatement.execute();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
 
     }
 
     //Удалить пользователя по ID (Delete)
     public void removeUserById(long id) {
-        Util util = new Util();
-        try {
-            Long x = id;
-            PreparedStatement prepareStatement = util.getConnection().prepareStatement("delete from table113 where id = ?;");
+        try (PreparedStatement prepareStatement = connection.prepareStatement("delete from table113 where id = ?;")) {
+            connection.setAutoCommit(false);
             prepareStatement.setLong(1, id);
             prepareStatement.execute();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     //Получить всех пользователей (Read)
     public List<User> getAllUsers() {
-        Util util = new Util();
         ArrayList<User> arr = new ArrayList<>();
-        try {
-            Statement statement = util.getConnection().createStatement();
+        try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
             ResultSet resultSet = statement.executeQuery("select * from table113;");
             while (resultSet.next()) {
                 User user = new User();
@@ -82,20 +98,31 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(resultSet.getByte(4));
                 arr.add(user);
             }
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
         return arr;
     }
 
     //Очистить таблицу пользователей (Delete)
     public void cleanUsersTable() {
-        Util util = new Util();
-        try {
-            Statement statement = util.getConnection().createStatement();
+        try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
             statement.executeUpdate("delete from table113;");
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,17 +3,14 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    Connection connection;
+    static Connection connection;
 
-    public Util() {
+    public static Connection getConnection() {
         try {
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/schema113", "root", "root");
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public Connection getConnection() {
         return connection;
     }
 }
